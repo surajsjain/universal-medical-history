62807cc1295943c06dbb8ae35dae74d27dd1873b
Suraj S Jain <surajsjain@hotmail.com>
2020-12-31 23:14:02 +0530

    added a user details model

--- usermgmt/models.py
+++ usermgmt/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserDetails(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    gender = models.CharField(max_length=15, null=True, blank=True)
+    date_of_birth = models.DateField(default= datetime.now())
+    blood_group = models.CharField(max_length=5, blank=True)
+
+    address = models.CharField(max_length=300)
+    city = models.CharField(max_length=20)
+    iso_country_code = models.CharField(max_length=5)
+
+    occupation = models.CharField(max_length=40, blank=True)
