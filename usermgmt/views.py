706855aaa8546d408e2ca57d59f916152abc6725
Suraj S Jain <surajsjain@hotmail.com>
2020-12-31 22:48:50 +0530

    Added a user management app

---
+++ usermgmt/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+
+# Create your views here.
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'pages/login.html')
+    else:
+        pass
