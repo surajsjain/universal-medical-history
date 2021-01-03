6f463fc06cf86d70fc96957a5e30a3d89cda1bca
Suraj S Jain <surajsjain@hotmail.com>
2021-01-03 22:20:08 +0530

    Added a visit model

--- medical_visit/models.py
+++ medical_visit/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
+class Visit(models.Model):
+    patient = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='patient')
+    doctor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='doctor')
+    date_time = models.DateTimeField(default=now)
+    completed = models.BooleanField(default=False)
