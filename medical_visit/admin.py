3a3ad065abbbe9b5ede85535dfb9c7355bdcb6ec
Suraj S Jain <surajsjain@hotmail.com>
2021-01-03 23:50:46 +0530

    Reg admin pan

--- medical_visit/admin.py
+++ medical_visit/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Visit)
+admin.site.register(GeneralCheckup)
+admin.site.register(TongueAndLipExamination)
+admin.site.register(SkinExamination)
+admin.site.register(Vaccine)
+admin.site.register(DrugPrescription)
+admin.site.register(TestPrescription)
