19cd32e64f575e4ae1cd9ea849799fe337182024
Suraj S Jain <surajsjain@hotmail.com>
2021-01-03 22:34:14 +0530

    Added a general checkup

--- medical_visit/models.py
+++ medical_visit/models.py
@@ -9,3 +9,9 @@ class Visit(models.Model):
     date_time = models.DateTimeField(default=now)
     completed = models.BooleanField(default=False)
     purpose = models.CharField(max_length=3000, default='')
+
+class GeneralCheckup(models.Model):
+    visit = models.ForeignKey(Visit, on_delete=models.SET_NULL, null=True)
+    body_temperature_in_fahrenheit = models.FloatField()
+    weight_in_kilograms = models.FloatField()
+    height_in_inches = models.FloatField()
