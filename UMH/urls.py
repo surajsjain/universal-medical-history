4ea9a350fe9fa3b845a7d4aed9573a366514f055
Suraj S Jain <surajsjain@hotmail.com>
2021-01-04 14:34:38 +0530

    URL structure for user_dash

--- UMH/urls.py
+++ UMH/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('', include('usermgmt.urls')),
     path('admin/', admin.site.urls),
+    path('user_dash/', include('user_dash.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
