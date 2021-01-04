a69d652c7b2c67b5092b5ddd7fb02bab53aeecdb
Suraj S Jain <surajsjain@hotmail.com>
2021-01-04 22:33:03 +0530

    Added a login mechanism

--- usermgmt/views.py
+++ usermgmt/views.py
@@ -5,6 +5,26 @@ from django.contrib import messages, auth
 # Create your views here.
 def login(request):
     if request.method == 'GET':
-        return render(request, 'auth/login.html')
-    else:
-        pass
+        if(request.user.is_authenticated):
+            # TODO: Check the user type (Doctor or Regular user) and redirect accordingly
+            return redirect('ud_mainDash')
+        else:
+            return render(request, 'auth/login.html')
+
+    elif request.method == 'POST':
+        data = request.POST
+
+        email = data['email']
+        password = data['password']
+
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                auth.login(request, user)
+                return redirect('ud_mainDash') # TODO: Check the user type (Doctor or Regular user) and redirect accordingly
+            else:
+                return redirect('login')
+
+        except:
+            print('could not get the user')
+            return redirect('login')
