f06ef1e2b72a7a7e8def90ac23771fc21743ef28
Suraj S Jain <surajsjain@hotmail.com>
2021-01-05 16:07:15 +0530

    Added a main dashboard page

--- user_dash/views.py
+++ user_dash/views.py
@@ -1,5 +1,36 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
+from django.db.models import Q
+
+from medical_visit.models import *
 
 # Create your views here.
 def mainDash(request):
-    return render(request, 'dashboard/user_dash/index.html')
+    ctxt = {}
+
+    doc_visits = Visit.objects.filter(Q(patient=request.user) & Q(completed = False))
+    ctxt['visits'] = doc_visits
+
+    drug_prescriptions = DrugPrescription.objects.filter(visit__patient = request.user)
+    current_prescriptions = []
+    for drug in drug_prescriptions:
+        time_limit = drug.visit.date_time + timedelta(days= drug.duration_in_days)
+        if(datetime.now().date() <= time_limit.date()):
+            current_prescriptions.append(drug)
+    ctxt['drug_prescriptions'] = current_prescriptions
+
+
+    test_prescriptions = TestPrescription.objects.filter(visit__patient = request.user)
+    pending_tests = []
+    for test in test_prescriptions:
+        try:
+            report = test.report
+            url = report.url
+        except:
+            pending_tests.append(test)
+    ctxt['test_prescriptions'] = pending_tests
+
+    print(ctxt)
+
+    return render(request, 'dashboard/user_dash/index.html', context=ctxt)
