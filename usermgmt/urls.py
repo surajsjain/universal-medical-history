b29e4e031397272516df6769b75d328a09c38ef3
Suraj S Jain <surajsjain@hotmail.com>
2021-01-05 12:22:00 +0530

    Added a sign-up mechanism

--- usermgmt/urls.py
+++ usermgmt/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- usermgmt/views.py
+++ usermgmt/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
 
+from .models import UserDetails
+
 # Create your views here.
 def login(request):
     if request.method == 'GET':
@@ -32,4 +34,42 @@ def login(request):
 def logout(request):
     if(request.method == 'POST'):
         auth.logout(request)
+        return redirect('login')
+
+def signup(request):
+    if(request.method == 'GET'):
+        return render(request, 'auth/signup.html')
+
+    elif(request.method == 'POST'):
+        data = request.POST
+
+        user = User.objects.create_user(username=data['username'], first_name=data['first_name'],
+                                        last_name=data['last_name'], email=data['email'],
+                                        password=data['password'])
+        user.save()
+
+        ud = UserDetails()
+        ud.user = user
+
+        if(data['mother'] != ''):
+            ud.mother = User.objects.get(id=data['mother'])
+        else:
+            ud.mother = None
+
+        if (data['father'] != ''):
+            ud.mother = User.objects.get(id=data['father'])
+        else:
+            ud.father = None
+
+        ud.is_doctor = data['is_doctor']
+        ud.gender = data['gender']
+        ud.date_of_birth = data['date_of_birth']
+        ud.blood_group = data['blood_group']
+        ud.address = data['address']
+        ud.city = data['city']
+        ud.iso_country_code = data['iso_country_code']
+        ud.occupation = data['occupation']
+
+        ud.save()
+
         return redirect('login')
