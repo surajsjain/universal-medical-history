473780dd2f7283596758a5f12ea073fa8bed3db8
Suraj S Jain <surajsjain@hotmail.com>
2021-01-06 15:21:31 +0530

    Added the medical history page

--- doctor_dash/views.py
+++ doctor_dash/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.shortcuts import render
 
 from medical_visit.models import *
+from usermgmt.models import UserDetails
 
 
 # Create your views here.
@@ -84,5 +85,24 @@ def medicalHistory(request):
     ctxt = {}
     ctxt['dash_type'] = 'doctor'
     ctxt['active'] = 'medical_history'
+
+    patients = Visit.objects.filter(doctor=request.user).values('patient').distinct()
+    patients_details = UserDetails.objects.filter(user__in=patients)
+
+
+    if(request.method == "POST"):
+        data = request.POST
+
+        if(data['category'] == "patient_name"):
+            patients_details = patients_details.filter(Q(user__first_name__icontains=data['search_term']) | Q(user__last_name__icontains=data['search_term']) | Q(user__username__icontains=data['search_term']))
+
+        elif(data['category'] == "patient_id"):
+            patients_details = patients_details.filter(user__id=data['search_term'])
+
+        elif(data['category'] == "city"):
+            patients_details = patients_details.filter(city__icontains=data['search_term'])
+
+
+    ctxt['patients'] = patients_details
 
     return render(request, 'dashboard/doctor_dash/medical_history.html', context=ctxt)
