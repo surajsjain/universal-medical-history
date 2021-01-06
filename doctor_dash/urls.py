4e499cbe9ab285ab01ebcdfd443177a52d14fcea
Suraj S Jain <surajsjain@hotmail.com>
2021-01-06 13:32:19 +0530

    Initiated the pages

--- doctor_dash/urls.py
+++ doctor_dash/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mainDash, name='dd_mainDash'),
+    path('', views.todayAppointments, name='dd_mainDash'),
+    path('all_appointments/', views.allAppointments, name='all_appointments'),
+    path('medical_history/', views.medicalHistory, name='patient_medical_history'),
 ]
--- doctor_dash/views.py
+++ doctor_dash/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-def mainDash(request):
+def todayAppointments(request):
     ctxt = {}
     ctxt['dash_type'] = 'doctor'
+    ctxt['active'] = 'today_appointments'
 
-    return render(request, 'dashboard/doctor_dash/index.html', context=ctxt)
+    return render(request, 'dashboard/doctor_dash/today_appointments.html', context=ctxt)
+
+
+def allAppointments(request):
+    ctxt = {}
+    ctxt['dash_type'] = 'doctor'
+    ctxt['active'] = 'all_appointments'
+
+    return render(request, 'dashboard/doctor_dash/all_appointments.html', context=ctxt)
+
+
+def medicalHistory(request):
+    ctxt = {}
+    ctxt['dash_type'] = 'doctor'
+    ctxt['active'] = 'medical_history'
+
+    return render(request, 'dashboard/doctor_dash/medical_history.html', context=ctxt)
